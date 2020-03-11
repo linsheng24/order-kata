44dd31120783a6e9ce38ac95ac185faf8c7b8cf1
linsheng24 <linsheng24@gmail.com>
2020-03-11 19:02:02 +0800

    Refactor Order

--- app/Order.php
+++ app/Order.php
@@ -4,23 +4,25 @@ namespace App;
 class Order
 {
 
+  private $menu = [
+    ["name" => "豬肉漢堡", "price" => 100], 
+    ["name" => "牛肉漢堡", "price" => 160], 
+    ["name" => "義大利麵", "price" => 130]
+  ];
   private $count = [0, 0, 0];
+  private $total = 0;
+
 
   public function add($value)
   {
     $this->count[$value]++;
+    $this->total = $this->menu[0]["price"] * $this->count[0] + $this->menu[1]["price"] * $this->count[1] + $this->menu[2]["price"] * $this->count[2];
   }
 
   public function getPrice()
   {
     
-    if ($this->count[0] == 1) {
-      return 100;
-    }
-    
-    if ($this->count[1] == 1) {
-      return 160;
-    }
+    return $this->total;
 
   }
   
